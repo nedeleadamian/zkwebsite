5bde822dc0fc1a6dc354811f1f7ceb14dd12d179
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-21 12:12:12 +1000

    chore: cull team

--- src/views/Page/index.vue
+++ src/views/Page/index.vue
@@ -526,46 +526,6 @@ export default {
         content: "Chief Executive Officer (CEO)",
         fadeInLeft: true,
       }, {
-        src: require('@/assets/imgs/img_tx_2@2x.png'),
-        name: "Emily Collins",
-        content: "Chief Technology Officer (CTO)",
-        fadeInLeft: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_3@2x.png'),
-        name: "Michael Rodriguez",
-        content: "Chief Operations Officer (COO)",
-        fadeInLeft: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_4@2x.png'),
-        name: "Sarah Thompson",
-        content: "Head of Cryptography",
-        fadeInRight: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_5@2x.png'),
-        name: "David Nguyen",
-        content: "Lead Blockchain Engineer",
-        fadeInRight: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_6@2x.png'),
-        name: "Rebecca White",
-        content: "Head of ProductHead of Product",
-        fadeInRight: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_7@2x.png'),
-        name: "James Wright",
-        content: "Head of Partnerships",
-        fadeInLeft: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_8@2x.png'),
-        name: "Olivia Martinez",
-        content: "Chief Marketing Officer (CMO)",
-        fadeInLeft: true,
-      }, {
-        src: require('@/assets/imgs/img_tx_9@2x.png'),
-        name: "Andrew Clark",
-        content: "Senior Smart Contract Developer",
-        fadeInLeft: true,
-      }, {
         src: require('@/assets/imgs/img_tx_10@2x.png'),
         name: "Natalie Foster",
         content: "Community Manager",
