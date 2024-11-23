e089bf0c266c4ca84e579520474656e9d457b9f9
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-23 11:59:52 +1000

    chore: clean up Home.vue for improved formatting and consistency

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,28 @@
 <template>
-    <div class="home">
-        
-            <!-- <top-bar v-if="permission.length===0" /> -->
-            <router-view class="home-body" />
-   
-    </div>
+  <div class="home">
+    <!-- <top-bar v-if="permission.length===0" /> -->
+    <router-view class="home-body" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Home',
-
-}
+  name: "Home",
+};
 </script>
 <style lang="scss">
-    .home {
-        width: 100%;
-         //overflow-x: hidden;
-        .home-content {
-            width: calc(100% - 140px);
-            position: relative;
-            flex-grow: 1;
-            .home-body{
-                background: #F8F9FC;
-                width: 100%;
-                overflow-x: hidden;
-            }
-        }
+.home {
+  width: 100%;
+  //overflow-x: hidden;
+  .home-content {
+    width: calc(100% - 140px);
+    position: relative;
+    flex-grow: 1;
+    .home-body {
+      background: #f8f9fc;
+      width: 100%;
+      overflow-x: hidden;
     }
+  }
+}
 </style>
