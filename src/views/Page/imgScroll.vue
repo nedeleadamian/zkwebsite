55354ef13a255198efd9c8cb409e63ece86ca0a9
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-23 11:59:20 +1000

    chore: refactor imgScroll.vue for improved readability and formatting

--- src/views/Page/imgScroll.vue
+++ src/views/Page/imgScroll.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <vue-seamless-scroll :data="list" class="seamless-warp-img" :class-option="optionSingleHeightTime">
+    <vue-seamless-scroll
+      :data="list"
+      class="seamless-warp-img"
+      :class-option="optionSingleHeightTime"
+    >
       <!-- 在需要添加点击事件的元素上绑定 :data-obj="JSON.stringify(item)" 否则事件无法触发且会抱错 -->
-      <div class="item " v-for="(item, index) in list" :key="index" :data-obj="JSON.stringify(item)">
+      <div
+        class="item"
+        v-for="(item, index) in list"
+        :key="index"
+        :data-obj="JSON.stringify(item)"
+      >
         <div class="swiperCon">
           <div class="left">
-            <img :src="item.one" alt="">
+            <img :src="item.one" alt="" />
           </div>
           <div class="right">
-            <img class="one" :src="item.two" alt="">
-            <img :src="item.three" alt="">
+            <img class="one" :src="item.two" alt="" />
+            <img :src="item.three" alt="" />
           </div>
         </div>
       </div>
     </vue-seamless-scroll>
   </div>
-
 </template>
 
 <script>
-import vueSeamlessScroll from 'vue-seamless-scroll'
+import vueSeamlessScroll from "vue-seamless-scroll";
 export default {
-  name: 'test',
+  name: "test",
   components: {
     vueSeamlessScroll,
   },
@@ -38,31 +46,36 @@ export default {
         singleHeight: 0, // 单步运动停止的高度(默认值0是无缝不停止的滚动)
         singleWidth: 0, // 单步运动停止的宽度(默认值0是无缝不停止的滚动)
         waitTime: 1000, // 单步运动停止的时间(默认值1000ms)
-      }
+      };
     },
   },
   data() {
     return {
-      list: [{
-        one: require('@/assets/imgs/img_zp1@2x.png'),
-        two: require('@/assets/imgs/img_zp2@2x.png'),
-        three: require('@/assets/imgs/img_zp3@2x.png'),
-      }, {
-        one: require('@/assets/imgs/img_zp4@2x.png'),
-        two: require('@/assets/imgs/img_zp5@2x.png'),
-        three: require('@/assets/imgs/img_zp6@2x.png'),
-      }, {
-        one: require('@/assets/imgs/img_zp1@2x.png'),
-        two: require('@/assets/imgs/img_zp2@2x.png'),
-        three: require('@/assets/imgs/img_zp3@2x.png'),
-      }, {
-        one: require('@/assets/imgs/img_zp4@2x.png'),
-        two: require('@/assets/imgs/img_zp5@2x.png'),
-        three: require('@/assets/imgs/img_zp6@2x.png'),
-      },],
-    }
+      list: [
+        {
+          one: require("@/assets/imgs/img_zp1@2x.png"),
+          two: require("@/assets/imgs/img_zp2@2x.png"),
+          three: require("@/assets/imgs/img_zp3@2x.png"),
+        },
+        {
+          one: require("@/assets/imgs/img_zp4@2x.png"),
+          two: require("@/assets/imgs/img_zp5@2x.png"),
+          three: require("@/assets/imgs/img_zp6@2x.png"),
+        },
+        {
+          one: require("@/assets/imgs/img_zp1@2x.png"),
+          two: require("@/assets/imgs/img_zp2@2x.png"),
+          three: require("@/assets/imgs/img_zp3@2x.png"),
+        },
+        {
+          one: require("@/assets/imgs/img_zp4@2x.png"),
+          two: require("@/assets/imgs/img_zp5@2x.png"),
+          three: require("@/assets/imgs/img_zp6@2x.png"),
+        },
+      ],
+    };
   },
-}
+};
 </script>
 
 <style lang="scss">
@@ -105,7 +118,7 @@ export default {
   }
 }
 
-.seamless-warp-img>div {
+.seamless-warp-img > div {
   display: flex;
 }
 </style>
